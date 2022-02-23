9744b4bb33940db662642ee695e297ac26106b3c
sakura <1718089268@qq.com>
2022-02-23 18:03:35 +0800

    updata README setup

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pycqBot",
-    version="0.1.0",
+    version="0.2.0",
     description="go-cqhttp python 框架，可以用于快速塔建 bot",
     long_description=long_description,
     long_description_content_type="text/markdown",
